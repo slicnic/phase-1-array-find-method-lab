713e94f981edd7f036a520a16409e5102eaaf746
Nic <115425395+slicnic@users.noreply.github.com>
2022-11-07 14:24:54 -0700

    commit tree d0828c4facef9601ae6c9f45bd39f8352dc8e321

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+]
+
+function superbowlWin (wins) {
+    return wins.result === "L" 
+
+}
+console.log(record.find(superbowlWin).year);
